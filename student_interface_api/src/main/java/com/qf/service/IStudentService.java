08b83b3b6f3fffeb5b972f2d34858a6dd946e2fb
pjy <123@qq.com>
2019-11-30 13:58:15 +0800

    cxCVv

--- student_interface_api/src/main/java/com/qf/service/IStudentService.java
+++ student_interface_api/src/main/java/com/qf/service/IStudentService.java
@@ -2,6 +2,6 @@ package com.qf.service;
 
 public interface IStudentService {
     public static void main(String[] args) {
-        System.out.println("ajhvffv");
+        System.out.println("123");
     }
 }
